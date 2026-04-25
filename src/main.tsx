60b9c6ef1dc23ff7c02b4d21b3658f04bc9d1b9a
abulaith87 <abulaithali@gmail.com>
2026-04-25 07:04:42 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function SafeApp() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
+
+// حماية من crash كامل
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<SafeApp />)
+} else {
+  document.body.innerHTML = `
+    <div style="color:white;background:black;padding:20px">
+      App failed to mount (root not found)
+    </div>
+  `
+}
